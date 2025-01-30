e966f3e2ce2fe0b0310d874dd4de0b0fecf82332
shaeq <s.nurin@student.han.nl>
2025-01-30 14:33:24 +0100

    link weg en logo

--- applicatie/index.php
+++ applicatie/index.php
@@ -25,7 +25,6 @@ include 'header.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Menu - Pizzeria Sole Machina</title>
     <link rel="stylesheet" href="css/style.css">
-    <link href="https://fonts.googleapis.com/css2?family=Pacifico&family=Nunito+Sans:wght@400;700&display=swap" rel="stylesheet">
 </head>
 <body>
     <main>
