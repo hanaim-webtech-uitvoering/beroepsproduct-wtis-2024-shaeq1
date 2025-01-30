e6c690659b4420ee5b8b76f4e3cdf9ab8dcf18f8
shaeq <s.nurin@student.han.nl>
2025-01-30 13:49:30 +0100

    winkelmandje toegevoegd

--- applicatie/header.php
+++ applicatie/header.php
@@ -1,38 +1,38 @@
 <?php
-session_start();
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 $isPersoneel = isset($_SESSION['role']) && $_SESSION['role'] === 'personeel';
 $currentPage = basename($_SERVER['PHP_SELF']);
 ?>
-<!DOCTYPE html>
-<html lang="nl" dir="ltr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?= $title ?? 'Pizzeria Sole Machina' ?></title>
-    <link rel="stylesheet" href="css/style.css">
-    <link href="https://fonts.googleapis.com/css2?family=Pacifico&family=Nunito+Sans:wght@400;700&display=swap" rel="stylesheet">
-</head>
-<body>
-    <header>
-        <img src="images/logo.png" alt="Pizzeria Logo" class="logo">
-        <nav>
-            <ul>
-                <li><a href="index.php" <?= $currentPage === 'index.php' ? 'class="active"' : '' ?>>Menu</a></li>
-                
-                <?php if ($isPersoneel): ?>
-                    <li><a href="bestellingen-personeel.php" <?= $currentPage === 'bestellingen-personeel.php' ? 'class="active"' : '' ?>>Bestellingen</a></li>
-                <?php endif; ?>
 
-                <?php if (isset($_SESSION['ingelogd'])): ?>
-                    <li><a href="profiel.php" <?= $currentPage === 'profiel.php' ? 'class="active"' : '' ?>>Profiel</a></li>
-                    <li>
-                        <form method="post" action="logout.php">
-                            <button type="submit" class="uitlog-knop">Uitloggen</button>
-                        </form>
-                    </li>
-                <?php else: ?>
-                    <li><a href="login.php" <?= $currentPage === 'login.php' ? 'class="active"' : '' ?>>Inloggen</a></li>
-                <?php endif; ?>
-            </ul>
-        </nav>
-    </header>
+<header>
+    <nav>
+        <ul>
+            <li class="logo-item">
+                <img src="images/logo.png" alt="Logo" class="logo">
+            </li>
+            <li><a href="index.php" <?= $currentPage === 'index.php' ? 'class="active"' : '' ?>>Menu</a></li>
+            
+            <?php if ($isPersoneel): ?>
+                <li><a href="bestellingen-personeel.php" <?= $currentPage === 'bestellingen-personeel.php' ? 'class="active"' : '' ?>>Bestellingen</a></li>
+            <?php endif; ?>
+
+            <?php if (isset($_SESSION['ingelogd'])): ?>
+                <li><a href="profiel.php" <?= $currentPage === 'profiel.php' ? 'class="active"' : '' ?>>Profiel</a></li>
+            <?php else: ?>
+                <li><a href="login.php" <?= $currentPage === 'login.php' ? 'class="active"' : '' ?>>Inloggen</a></li>
+            <?php endif; ?>
+
+            <!-- Winkelmandje rechtsboven -->
+            <li class="winkelmandje-item">
+                <a href="winkelmandje.php">
+                    <img src="images/winkelmandje.png" alt="Winkelmandje" class="winkelmandje-logo">
+                    <?php if (!empty($_SESSION['winkelmandje'])): ?>
+                        <span class="winkelmandje-aantal"><?= count($_SESSION['winkelmandje']) ?></span>
+                    <?php endif; ?>
+                </a>
+            </li>
+        </ul>
+    </nav>
+</header>
