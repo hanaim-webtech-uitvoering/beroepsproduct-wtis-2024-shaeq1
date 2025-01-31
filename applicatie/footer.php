74eccd24ba6f1ddcf58cc67fc8c17dc6801149a9
shaeq <s.nurin@student.han.nl>
2025-01-31 17:51:47 +0100

    html validotro

--- applicatie/footer.php
+++ applicatie/footer.php
@@ -1,6 +1,16 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pizzeria Sole Machina</title>
+</head>
+<body>
+
 <footer>
-    <p>&copy; <?= date('Y') ?> Pizzeria Sole Machina</p>
+    <p>&copy; <?php echo date('Y'); ?> Pizzeria Sole Machina</p>
     <a href="privacy.php">Privacyverklaring</a>
 </footer>
+
 </body>
 </html>
